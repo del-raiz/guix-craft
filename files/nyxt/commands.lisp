d7f2b32a579b5bcdd38cba6ccf86b9957a601252
Erik Almaraz <erikalmaraz@fastmail.com>
2025-01-27 19:10:54 -0800

    Add micros server to connect to Lem.

---
+++ files/nyxt/commands.lisp
@@ -0,0 +1,28 @@
+;;;; Nyxt Commands
+
+;;; Commentary:
+;;;
+
+;;; References
+;;; 1.
+;;; 2.
+
+(in-package :nyxt)
+
+(require :micros)
+
+(defvar *micros-port* 4006
+  "Default Common Lisp server port for Nyxt")
+
+;; Lem connection to Nyxt
+
+(define-command start-micros (&optional (micros-port *micros-port*))
+  "Start a Micros server enabling connecting to Lem via SLIME."
+  (micros:create-server :port micros-port :dont-close t)
+  (echo "Micros server started at port ~a" micros-port))
+
+(define-command stop-micros (&optional (micros-port *micros-port*))
+  "Stop current Micros server."
+  (micros:stop-server micros-port)
+  (echo "Closing Micros server at port ~a" micros-port))
+
--- files/nyxt/config.lisp
+++ files/nyxt/config.lisp
@@ -19,24 +19,51 @@
 ;;; everywhere.
 #+(or nyxt-3 nyxt-4) (reset-asdf-registries)
 
+(define-configuration buffer
+  ((default-modes `(emacs-mode ,@%slot-value%))))
+
 ;; Loading files from the same directory (~/.config/nyxt/).
 (define-nyxt-user-system-and-load nyxt-user/basic-config
-  :components ("utilities"
-               "keepassxc-pwi"
+  :description "Nyxt Interface Configuration."
+  :components ("keepassxc-pwi"
                "keepassxc-3431"))
 
-(define-configuration buffer
-    ((default-modes `(emacs-mode ,@%slot-value%))))
-
 ;;; Nyxt Extensions
-;;; ~/.local/share/nyxt/extensions/*
+
+;; ~/.local/share/nyxt/extensions/*
 (define-nyxt-user-system-and-load nyxt-user/nx-invader-2-proxy
   :description "Simple Dark style theme for Nyxt"
   :depends-on ("nx-invader-2"))
 
+
 ;;; Hacks
+
 ;;Borrowed from aartaka
 (defmethod files:resolve ((profile nyxt:nyxt-profile) (file nyxt/mode/bookmark:bookmarks-file))
            "Reroute bookmarks to the `.config/nyxt/' directory."
            #p"~/.config/nyxt/bookmarks.lisp")
+
+;; Setup Micros Server to connect to Lem.
+
+;; (define-nyxt-user-system-and-load nyxt-user/commands
+;;   :description "Custom Nyxt Commands."
+;;   :components ("commands"))
+
+(in-package :nyxt)
+(require :micros)
+
+(defvar *micros-port* 4006
+  "Default Common Lisp server port for Nyxt")
+
+;; Lem connection to Nyxt
+
+(define-command start-micros (&optional (micros-port *micros-port*))
+    "Start a Micros server enabling connecting to Lem via SLIME."
+  (micros:create-server :port micros-port :dont-close t)
+  (echo "Micros server started at port ~a" micros-port))
+
+(define-command stop-micros (&optional (micros-port *micros-port*))
+    "Stop current Micros server."
+  (micros:stop-server micros-port)
+  (echo "Closing Micros server at port ~a" micros-port))
 
